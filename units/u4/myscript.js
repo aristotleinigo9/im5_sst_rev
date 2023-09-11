dbf357b724fe7aafb1d4af2d1921bd9cf8ae6bf5
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-11 09:20:24 +0300

    Update myscript.js

--- units/u4/myscript.js
+++ units/u4/myscript.js
@@ -11,6 +11,128 @@ var myQuestions = [
     correctAnswer: 'b'
   },
 
+  {
+    question: "<h4>2. The graph below shows the average production of oil (in million barrels per day) in selected oil fields from different regions. How many barrels per day is produced in regions Alfa and Carlie?</h4> <img src='images/1_bar.png' width='500' height='210'>",
+    answers: {
+      a: '\\({5}\\)<br>',
+      b: '\\({500,000}\\)<br>',
+      c: '\\({5,000,000}\\)<br>',
+      d: '\\({5,000}\\)',
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "<h4>3. What is the change of temperature from 8:00 AM to 3:00 PM?</h4> <img src='images/2_table.png' width='550' height='220'>",
+    answers: {
+      a: '\\({10}~{^oC}\\)<br>',
+      b: '\\({33}~{^oC}\\)<br>',
+      c: '\\({42}~{^oC}\\)<br>',
+      d: '\\({9}~{^oC}\\)<br>',
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<h4>4. The circle graph below shows the percentage production of oil in the four regions in one day. The total oil produced was \\({30}~{million}~{barrels}\\). How many barrels of oil was produced in the East region?</h4> <img src='images/3_circle.png' width='200' height='200'>",
+    answers: {
+      a: '\\({9,000,000}\\)<br>',
+      b: '\\({900,000}\\)<br>',
+      c: '\\({6,000,000}\\)<br>',
+      d: '\\({4,500,0000}\\)<br>',
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>5. The circle graph below shows the percentage production of oil in the four regions in one day. The total oil produced was \\({30}~{million}~{barrels}\\). What is the measure of the central angle equivalent to the North region oil production?</h4> <img src='images/3_circle.png' width='200' height='200'>",
+    answers: {
+      a: '\\({126^o}\\)<br>',
+      b: '\\({33^o}\\)<br>',
+      c: '\\({42^o}\\)<br>',
+      d: '\\({72^o}\\)<br>',
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "6. Calculate the mean of \\({63.5}~{cm}\\), \\({36.5}~{cm}\\), \\({32.6}~{cm}\\), \\({23.4}~{cm}\\), and \\({60.5}~{cm}\\). Round your answer to one decimal place.",
+    answers: {
+      a: '\\({43.5}~{cm}\\)<br>',
+      b: '\\({43.3}~{cm}\\)<br>',
+      c: '\\({45.3}~{cm}\\)<br>',
+      d: '\\({45.5}~{cm}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "7. Find the average of \\({36.2}~{m}\\), \\({56.8}~{m}\\), \\({13.4}~{m}\\), \\({43.9}~{m}\\),\\({57.3}~{m}\\), and \\({26.2}~{m}\\). Round your answer to one decimal place.",
+    answers: {
+      a: '\\({39.0}~{m}\\)<br>',
+      b: '\\({38.9}~{m}\\)<br>',
+      c: '\\({46.8}~{m}\\)<br>',
+      d: '\\({46.7}~{m}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "8. Determine the median of \\({623.5}~{m}\\), \\({326.5}~{m}\\), \\({322.6}~{m}\\), \\({223.4}~{m}\\), and \\({620.5}~{m}\\). Round your answer to one decimal place.",
+    answers: {
+      a: '\\({326.5}~{m}\\)<br>',
+      b: '\\({362.5}~{m}\\)<br>',
+      c: '\\({322.6}~{m}\\)<br>',
+      d: '\\({223.4}~{m}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "9. Determine the median of \\({23.5}~{in}\\), \\({26.5}~{in}\\), \\({22.6}~{in}\\), \\({22.5}~{in}\\), \\({23.4}~{in}\\), and \\({20.5}~{in}\\). Round your answer to one decimal place.",
+    answers: {
+      a: '\\({22.6}~{in}\\)<br>',
+      b: '\\({23.4}~{in}\\)<br>',
+      c: '\\({23.0}~{in}\\)<br>',
+      d: '\\({23.5}~{in}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "10. Identify the mode/s on the following resistances: \\({12}\\Omega\\),\\({}\\) \\({15}\\Omega\\),\\({}\\) \\({13}\\Omega\\),\\({}\\) \\({12}\\Omega\\),\\({}\\) \\({14}\\Omega\\),\\({}\\) \\({12}\\Omega\\),\\({}\\) \\({14}\\Omega\\),\\({}\\) \\({14}\\Omega\\),\\({}\\) \\({15}\\Omega\\), and \\({13}\\Omega\\).",
+    answers: {
+      a: '\\({12}\\Omega\\)<br>',
+      b: '\\({14}\\Omega\\)<br>',
+      c: '\\({12}\\Omega\\) and \\({13}\\Omega\\)<br>',
+      d: '\\({12}\\Omega\\) and \\({14}\\Omega\\)'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "11. The number of hours spent by a pipefitter in connecting pipes for the first five days of the week were \\({7}\\), \\({9}\\), \\({5}\\), \\({12}\\), and \\({8}\\). How many hours should he spend on the sixth day of the week to have a daily average of 8 hours?",
+    answers: {
+      a: '\\({8}\\)<br>',
+      b: '\\({7}\\)<br>',
+      c: '\\({6}\\)<br>',
+      d: '\\({5}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "12. Ahmed received the following grades on the first three quizzes in a math class: \\({92}\\), \\({95}\\), and \\({93}\\). What should be his grade on the fourth quiz so that his average will be 94?",
+    answers: {
+      a: '\\({96}\\)<br>',
+      b: '\\({95}\\)<br>',
+      c: '\\({98}\\)<br>',
+      d: '\\({97}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
 ];
 
 var quizContainer = document.getElementById('quiz');
