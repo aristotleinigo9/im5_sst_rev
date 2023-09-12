8f47336b4e902f870d989ce3cc193a9452cc5e2b
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-12 15:13:17 +0300

    Update myscript.js

--- units/u6/myscript.js
+++ units/u6/myscript.js
@@ -7,7 +7,217 @@ var myQuestions = [
       c: '\\({19}\\)<br>',
       d: '\\({14}\\)'
     },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "2. Solve: \\({3x-11=5x-31}\\).",
+    answers: {
+      a: '\\({5}\\)<br>',
+      b: '\\({10}\\)<br>',
+      c: '\\({8}\\)<br>',
+      d: '\\({21}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "3. Solve: \\({3(2x-3)+(5x+8)=(x-9)-2(3x+20)}\\).",
+    answers: {
+      a: '\\({-4}\\)<br>',
+      b: '\\({-3}\\)<br>',
+      c: '\\({-2}\\)<br>',
+      d: '\\({3}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "4. Solve for \\({R}\\) in the formula \\({V=IR}\\).",
+    answers: {
+      a: '\\({V\\over I}\\)<br>',
+      b: '\\({VR}\\)<br>',
+      c: '\\({I\\over V}\\)<br>',
+      d: '\\({VI}\\)<br>'
+    },
     correctAnswer: 'a'
+  },
+
+  {
+    question: "5. Solve for \\({r}\\) in the formula \\({V=2}\\pi\\)\\({r^2}{h}\\).",
+    answers: {
+      a: '\\({\\sqrt {2V\\over\\pi\{h}}}\\)<br>',
+      b: '\\({\\sqrt {Vh\\over{2}\\pi\}}\\)<br>',
+      c: '\\({\\sqrt {V\\pi\\\over\{2h}}}\\)<br>',
+      d: '\\({\\sqrt {V\\over2\\pi\{h}}}\\)<br>'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "6. Solve for \\({a}\\) in the formula \\({A=}\\) \\({h(a+b)\\over 2}\\).",
+    answers: {
+      a: '\\({bh\\over 2A}\\)<br>',
+      b: '\\({2A+b\\over h}\\)<br>',
+      c: '\\({2A-bh\\over h}\\)<br>',
+      d: '\\({2A-b\\over h}\\)<br>'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "7. In a parallel circuit, the total of three resistances (\\({R_T}\\)) is given by the formula: \\({1\\over R_T} = {1\\over R_1} + {1\\over R_2}+ {1\\over R_3}\\). Find the total resistance (\\({R_T}\\))  given that \\({R_1} = 8\\Omega\\), \\({R_2} = 12\\Omega\\), and \\({R_3} = 6\\Omega\\).",
+    answers: {
+      a: '\\(2{1\\over 3}\\Omega\\)<br>',
+      b: '\\(3{2\\over 3}\\Omega\\)<br>',
+      c: '\\(2{2\\over 3}\\Omega\\)<br>',
+      d: '\\(3{1\\over 3}\\Omega\\)'
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "8. Given that \\({r=3.2}\\), \\({s=5.1}\\), and \\({t=1.7}\\), find \\({u}\\) using the formula \\({r(s-t)\\over 8u}\\)\\({=4(t-s)}\\).",
+    answers: {
+      a: '\\(-{1\\over 12}\\)<br>',
+      b: '\\(-{1\\over 10}\\)<br>',
+      c: '\\({1\\over 10}\\)<br>',
+      d: '\\({1\\over 32}\\)<br>'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "9. A thermometer shows a reading of \\({40}~{^oC}\\). What is the equivalent temperature reading in \\({^oF}\\). Use \\({^oF} = {^oC}\\times\ 1.8{+32}\\).",
+    answers: {
+      a: '\\({104}~{^oF}\\)<br>',
+      b: '\\({120}~{^oF}\\)<br>',
+      c: '\\({100}~{^oF}\\)<br>',
+      d: '\\({116}~{^oF}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "10. A trapezoid has bases of \\({14}~{cm}\\) and \\({20}~{cm}\\). If its height is \\({10}~{cm}\\), what is its area (\\({A}\\))? Use \\({A=}{h(b_1+b_2)\\over 2}\\).",
+    answers: {
+      a: '\\({1,400 cm^2}\\)<br>',
+      b: '\\({170 cm^2}\\)<br>',
+      c: '\\({140 cm^2}\\)<br>',
+      d: '\\({240 cm^2}\\)<br>'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "11. What is the radius (\\({r}\\)) of a sphere with the volume (\\({V}\\)) of \\({113.1}~{in^3}\\)? Round your answer to the nearest inch. Use the formula \\({V=}{4\\over 3}\\pi\\)\\({r^3}\\).",
+    answers: {
+      a: '\\({4}~{in}\\)<br>',
+      b: '\\({1}~{in}\\)<br>',
+      c: '\\({2}~{in}\\)<br>',
+      d: '\\({3}~{in}\\)<br>',
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "12. If \\({3x-5y=32}\\), find \\({y}\\) when \\({x=4}\\).",
+    answers: {
+      a: '\\({4}\\)<br>',
+      b: '\\({5}\\)<br>',
+      c: '\\({-5}\\)<br>',
+      d: '\\({-4}\\)'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "13. Which of the following is a solution to \\({2x-6y=-16}\\)?",
+    answers: {
+      a: '\\({(1,3)}\\)<br>',
+      b: '\\({(-1,3)}\\)<br>',
+      c: '\\({(-1,-3)}\\)<br>',
+      d: '\\({(1,-3)}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "14. If \\({y=}{3\\over4\}x-4\\), what is the value of \\({x}\\) when \\({y=2}\\)?",
+    answers: {
+      a: '\\({4}\\)<br>',
+      b: '\\({16}\\)<br>',
+      c: '\\({8}\\)<br>',
+      d: '\\({12}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "15. Which of the following is the solution on the given systems of linear equations? <p style='margin-left:5em'> \\({2x-3y=13}\\)</p> <p style='margin-left:5em'> \\({3x+6y=-12}\\)</p>",
+    answers: {
+      a: '\\({(2,3)}\\)<br>',
+      b: '\\({(2,-3)}\\)<br>',
+      c: '\\({(-2,-3)}\\)<br>',
+      d: '\\({(-2,3)}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "16. Find the value of \\(x\\) and \\(y\\) that satisfy the two linear equations below. <p style='margin-left:5em'> \\(y=3x-2\\)</p> <p style='margin-left:5em'> \\(13x+2y=15\\)</p>",
+    answers: {
+      a: '\\({(3,7)}\\)<br>',
+      b: '\\({(-2,-8)}\\)<br>',
+      c: '\\({(1,1)}\\)<br>',
+      d: '\\({(-1,-5)}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "17. Solve the system of linear equations below. <p style='margin-left:5em'> \\({2\\over3}x-{1\\over3}y = 3\\)</p> <p style='margin-left:5em'> \\(3x+5y=-6\\)</p>",
+    answers: {
+      a: '\\({(3,-3)}\\)<br>',
+      b: '\\({(-{2\\over3},-1)}\\)<br>',
+      c: '\\({(-1,-{3\\over5})}\\)<br>',
+      d: '\\({(-2,0)}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "18. The sum of the lengths of two bars is \\({45}~{ft}\\), while their difference is \\({11}~{ft}\\). How long is the smaller bar?",
+    answers: {
+      a: '\\({18}~{ft}\\)<br>',
+      b: '\\({17}~{ft}\\)<br>',
+      c: '\\({11}~{ft}\\)<br>',
+      d: '\\({15}~{ft}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "19. Four \\({15g-sodium}~{metal}\\), and five \\({20g-chlorine}~{tablets}\\) cost \\($108\\) while three \\({15g-sodium}~{metal}\\) and ten \\({20g-chlorine}~{tablets}\\) cost \\($106\\). Find the cost of a \\({15g-sodium}~{metal}\\).",
+    answers: {
+      a: '\\({$22}\\)<br>',
+      b: '\\({$20}\\)<br>',
+      c: '\\({$5}\\)<br>',
+      d: '\\({$4}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "20. A plumber cuts an \\({820}~{cm}\\) PVC into six pieces (4 small and 2 large pieces). If the larger pipe is \\({10}~{cm}\\) longer than twice the smaller pipe, how long is the large pipe?",
+    answers: {
+      a: '\\({100}~{cm}\\)<br>',
+      b: '\\({200}~{cm}\\)<br>',
+      c: '\\({220}~{cm}\\)<br>',
+      d: '\\({210}~{cm}\\)'
+    },
+    correctAnswer: 'd'
   },
   
 ];
