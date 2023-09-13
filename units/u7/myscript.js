64addea1a202cddc428c8148beb17cc9ab5b3f70
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-13 14:12:10 +0300

    Update myscript.js

--- units/u7/myscript.js
+++ units/u7/myscript.js
@@ -1,7 +1,7 @@
 var myQuestions = [
   
   {
-    question: "<h4>1. What is the perimeter and area of the triagnle below? Apply rules on measurement.</h4> <img src='images/1_triangle.png' width='200' height='150'>",
+    question: "<h4>1. What is the perimeter and area of the triangle below? Apply rules on measurement.</h4> <img src='images/1_triangle.png' width='200' height='150'>",
     answers: {
       a: '\\({P=32 cm, A = 48 cm^2}\\)<br>',
       b: '\\({P=40.0 cm, A=96.0 cm^2}\\)<br>',
@@ -10,6 +10,225 @@ var myQuestions = [
 
     },
     correctAnswer: 'c'
+  },
+
+  {
+    question: "<h4>2. What is the perimeter and area of the rectangle below? Apply rules on measurement.</h4> <img src='images/2_rectangle.png' width='250' height='150'>",
+    answers: {
+      a: '\\({P=22.3m, A = 123 m^2}\\)<br>',
+      b: '\\({P=44.6m, A=123.22 m^2}\\)<br>',
+      c: '\\({P=44.6m, A=123.2m^2}\\)<br>',
+      d: '\\({P=44.6m,A=123m^2}\\)',
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<h4>3. Find the perimeter and area of the trapezoid below? Apply rules on measurement.</h4> <img src='images/3_trapezoid.png' width='250' height='150'>",
+    answers: {
+      a: '\\({P=44.0ft, A = 110ft^2}\\)<br>',
+      b: '\\({P=44.0ft, A=110.0ft^2}\\)<br>',
+      c: '\\({P=44.01ft, A=109ft^2}\\)<br>',
+      d: '\\({P=44ft,A=109.9595ft^2}\\)',
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>4. Find the perimeter and area of the square below? Apply rules on measurement.</h4> <img src='images/4_square.png' width='200' height='150'>",
+    answers: {
+      a: '\\({P=92cm, A = 529cm^2}\\)<br>',
+      b: '\\({P=92cm, A=530cm^2}\\)<br>',
+      c: '\\({P=92.0cm, A=530cm^2}\\)<br>',
+      d: '\\({P=90.0cm,A=529cm^2}\\)',
+
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "<h4>5. Find the circumference and area of the circle below? Apply rules on measurement. Use \\(\\pi\=3.14\\).</h4> <img src='images/5_circle.png' width='150' height='150'>",
+    answers: {
+      a: '\\({C=14m, A = 17m^2}\\)<br>',
+      b: '\\({C=14.444m, A=16.6106m^2}\\)<br>',
+      c: '\\({C=14m, A=16m^2}\\)<br>',
+      d: '\\({C=14.4m,A=16.6m^2}\\)',
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>6. Find the circumference and area of the circle below? Apply rules on measurement. Use \\(\\pi\=3.14\\).</h4> <img src='images/6_circle.png' width='150' height='150'>",
+    answers: {
+      a: '\\({C=51.182ft, A = 208.57ft^2}\\)<br>',
+      b: '\\({C=51.18ft, A=208.6ft^2}\\)<br>',
+      c: '\\({C=51.2ft, A=209ft^2}\\)<br>',
+      d: '\\({C=51ft,A=210ft^2}\\)',
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "7. What is the radius of a circle with an area of \\({235}~{in^2}\\)? Apply rules on measurement. Use \\(\\pi\{=}{22\\over7}\\)",
+    answers: {
+      a: '\\({8.65}~{in}\\)<br>',
+      b: '\\({8.6}~{in}\\)<br>',
+      c: '\\({17.2}~{in}\\)<br>',
+      d: '\\({17.3}~{in}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "8. Determine the diameter of a circle whose circumference is \\({62.8}~{cm}\\)? Apply rules on measurement. Use \\(\\pi\=3.14\\).",
+    answers: {
+      a: '\\({20}~{cm}\\)<br>',
+      b: '\\({20.0}~{cm}\\)<br>',
+      c: '\\({19.9}~{cm}\\)<br>',
+      d: '\\({19}~{cm}\\)'
+    },
+    correctAnswer: 'c'
+  },
+  
+  {
+    question: "9. Looking at the side of a welded metal storage bin, the shape is a trapezoid. The length of the bases are \\({15.8}~{cm}\\) and \\({22.2}~{cm}\\), and the height is \\({13.4}~{cm}\\). Find its area. Apply rules on measurement.",
+    answers: {
+      a: '\\({245.6}~{cm^2}\\)<br>',
+      b: '\\({250.0}~{cm^2}\\)<br>',
+      c: '\\({255}~{cm^2}\\)<br>',
+      d: '\\({509}~{cm^2}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "10. Each hospital bed and its accessories use \\({108}~{ft^2}\\) of floor space. How many beds can be placed in a ward \\({27}~{ft}\\) by \\({36}~{ft}\\)?",
+    answers: {
+      a: '\\({8}\\)<br>',
+      b: '\\({7}\\)<br>',
+      c: '\\({10}\\)<br>',
+      d: '\\({9}\\)'
+    },
+    correctAnswer: 'd'
+  },
+  
+  {
+    question: "11. A rectangular piece of sheet metal has an area of \\({15,680}~{in^2}\\).Its length is \\({172}~{in}\\). Find its width. Apply rules on measurement.",
+    answers: {
+      a: '\\({91.16}~{in}\\)<br>',
+      b: '\\({91.2}~{in}\\)<br>',
+      c: '\\({90.0}~{in}\\)<br>',
+      d: '\\({91.0}~{in}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "12. A room has a floor dimension of \\({29ft} \\times\\ {46ft}\\).How many \\({2ft}{~}by{~}{2ft}\\) square carpet tiles are needed to cover the floor of two same-sized rooms?",
+    answers: {
+      a: '\\({667}\\)<br>',
+      b: '\\({670}\\)<br>',
+      c: '\\({333}\\)<br>',
+      d: '\\({334}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>13. Find the total surface area of the rectangular prism below. Apply rules on measurement.</h4> <img src='images/7_prism.png' width='175' height='250'>",
+    answers: {
+      a: '\\({TA = 1,260}~{cm^2}\\)<br>',
+      b: '\\({TA=1,262}~{cm^2}\\)<br>',
+      c: '\\({TA=1,262.48}~{cm^2}\\)<br>',
+      d: '\\({TA=1,262.5}~{cm^2}\\)',
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>13. Find the total surface area of the rectangular prism below. Apply rules on measurement.</h4> <img src='images/9_prism.png' width='200' height='175'>",
+    answers: {
+      a: '\\({54}~{sq.in.}\\)<br>',
+      b: '\\({72}~{sq.in.}\\)<br>',
+      c: '\\({94}~{sq.in.}\\)<br>',
+      d: '\\({110}~{sq.in.}\\)',
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<h4>14. Find the volume of the rectangular prism below. Apply rules on measurement.</h4> <img src='images/7_prism.png' width='175' height='250'>",
+    answers: {
+      a: '\\({V = 2,680}~{cm^3}\\)<br>',
+      b: '\\({V=2,700}~{cm^3}\\)<br>',
+      c: '\\({V=176}~{cm^3}\\)<br>',
+      d: '\\({V=180}~{cm^3}\\)',
+
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>15. Find the total surface area of the cylinder below. Apply rules on measurement. Use \\(\\pi\=3.14\\).</h4> <img src='images/8_cylinder.png' width='175' height='250'>",
+    answers: {
+      a: '\\({TA=156}~{in^2}\\)<br>',
+      b: '\\({TA=153}~{in^2}\\)<br>',
+      c: '\\({TA=594}~{in^2}\\)<br>',
+      d: '\\({TA=593}~{in^2}\\)',
+
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "<h4>16. Find the volume of the cylinder below. Apply rules on measurement. Use \\(\\pi\=3.14\\).</h4> <img src='images/8_cylinder.png' width='175' height='250'>",
+    answers: {
+      a: '\\({V=85.1}~{ft^3}\\)<br>',
+      b: '\\({V=85.0}~{ft^3}\\)<br>',
+      c: '\\({V=0.591}~{ft^3}\\)<br>',
+      d: '\\({V=0.591}~{ft^3}\\)',
+
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "17. The base of a cylindrical tank has a radius of \\({10.5}~{m}\\) and a height of \\({6.5}{~}{m}\\). Find the volume of the cylindrical tank. Apply rules on measurement. Use \\(\\pi\=3.14\\).",
+    answers: {
+      a: '\\({2,300}~{m^3}\\)<br>',
+      b: '\\({2,250}~{m^3}\\)<br>',
+      c: '\\({2,3}\\bar\{0}\\)\\({0}~{m^3}\\)<br>',
+      d: '\\({2,200}~{m^3}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "18. The base of a cylindrical tank has a diameter of \\({60.0}~{ft}\\) and a height of \\({33.6}{~}{ft}\\). If the cost of refining crude oil into fuel is \\({SAR}~{3.75}\\) per \\({1}~{ft^3}\\) of crude oil, how much would be the cost of refining a whole cylindrical tank of crude oil? Round your answer to the nearest \\(SAR\\). Use \\(\\pi\=3.14\\).",
+    answers: {
+      a: '\\({SAR}~{94,954}\\)<br>',
+      b: '\\({SAR}~{356,076}\\)<br>',
+      c: '\\({SAR}~{356,000}\\)<br>',
+      d: '\\({SAR}~{95,000}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "19. Steel weighs \\({0.00786}~{kg/cm^3}\\). What is the weight of a rectangular piece of steel \\({0.794cm}{~}by{~}{30.5cm}{~}by{~}{50.8cm}\\)? Round your answer to the nearest kilogram.",
+    answers: {
+      a: '\\({9.7}~{kg}\\)<br>',
+      b: '\\({1}\\bar\{0}\\)\\({0}~{kg}\\)<br>',
+      c: '\\({156,517}~{kg}\\)<br>',
+      d: '\\({157,000}~{kg}\\)'
+    },
+    correctAnswer: 'b'
   },
 
 ];
