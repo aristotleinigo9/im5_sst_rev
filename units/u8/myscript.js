3658185f86ef52275caa0e92e34f614690d39d0a
aristotleinigo9 <99675563+aristotleinigo9@users.noreply.github.com>
2023-09-17 08:38:40 +0300

    Update myscript.js

--- units/u8/myscript.js
+++ units/u8/myscript.js
@@ -1,6 +1,6 @@
 var myQuestions = [
   {
-    question: "<h4>1.Find \\({cos A}\\) of the given right triangle below.</h4> <img src='images/1_trig.png' width='150' height='150'>",
+    question: "<h4>1. Find \\({cos}~{A}\\) in the given right triangle below.</h4> <img src='images/1_trig.png' width='150' height='150'>",
     answers: {
       a: '\\({a\\over b}\\)<br>',
       b: '\\({a\\over c}\\)<br>',
@@ -9,8 +9,186 @@ var myQuestions = [
     },
     correctAnswer: 'c'
   },
-  
 
+  {
+    question: "<h4>2. Determine \\({tan}~{B}\\) in the given right triangle below.</h4> <img src='images/1_trig.png' width='150' height='150'>",
+    answers: {
+      a: '\\({a\\over b}\\)<br>',
+      b: '\\({a\\over c}\\)<br>',
+      c: '\\({b\\over c}\\)<br>',
+      d: '\\({b\\over a}\\)'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<h4>3. What is \\({sin}~{A}\\) in the given right triangle below.</h4> <img src='images/1_trig.png' width='150' height='150'>",
+    answers: {
+      a: '\\({a\\over b}\\)<br>',
+      b: '\\({a\\over c}\\)<br>',
+      c: '\\({b\\over c}\\)<br>',
+      d: '\\({b\\over a}\\)'
+    },
+    correctAnswer: 'b'
+  },
+  
+  {
+    question: "<h4>4. What is the value of \\({sin}~{X}\\) in the given right triangle below? Apply rules on measurement.</h4> <img src='images/2_triangle.png' width='200' height='140'>",
+    answers: {
+      a: '\\({0.3580}\\)<br>',
+      b: '\\({0.358}\\)<br>',
+      c: '\\({1.360}\\)<br>',
+      d: '\\({0.3581}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "<h4>5. What is the value of \\({cos}~{X}\\) in the given right triangle below? Apply rules on measurement.</h4> <img src='images/2_triangle.png' width='200' height='140'>",
+    answers: {
+      a: '\\({0.934}\\)<br>',
+      b: '\\({0.358}\\)<br>',
+      c: '\\({1.07}\\)<br>',
+      d: '\\({2.79}\\)'
+    },
+    correctAnswer: 'a'
+  },
+ 
+  {
+    question: "<h4>6. What is the value of \\({tan}~{W}\\) in the given right triangle below? Apply rules on measurement.</h4> <img src='images/2_triangle.png' width='200' height='140'>",
+    answers: {
+      a: '\\({2.607}\\)<br>',
+      b: '\\({0.384}\\)<br>',
+      c: '\\({0.3835}\\)<br>',
+      d: '\\({2.61}\\)'
+    },
+    correctAnswer: 'd'
+  },
+
+  {
+    question: "<h4>7. Determine the value of \\({tan}~{R}\\) in the given right triangle below. Apply rules on measurement.</h4> <img src='images/3_triangle.png' width='170' height='160'>",
+    answers: {
+      a: '\\({0.458}\\)<br>',
+      b: '\\({1.00}\\)<br>',
+      c: '\\({0.708}\\)<br>',
+      d: '\\({1.41}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "8. What is the value of \
+    \\({cos}~{23.6^o}\\)? Round your answer\
+    to three significant digits.",
+    answers: {
+      a: '\\({0.400}\\)<br>',
+      b: '\\({0.916}\\)<br>',
+      c: '\\({0.437}\\)<br>',
+      d: '\\({2.29}\\)'
+    },
+    correctAnswer: 'b'
+  },
+ 
+  {
+    question: "9. Determine the value of \
+    \\({sin}~{54.8^o}\\). Round your answer\
+    to four significant digits.",
+    answers: {
+      a: '\\({0.7054}\\)<br>',
+      b: '\\({1.418}\\)<br>',
+      c: '\\({0.5764}\\)<br>',
+      d: '\\({0.8171}\\)'
+    },
+    correctAnswer: 'd'
+  },
+  
+  {
+    question: "10. Determine \\({angle}~{A}\\) if \\({cos}~{A}=0.12345\\). Round your answer to the nearest hundredth of a degree.",
+    answers: {
+      a: '\\({28.19^o}\\)<br>',
+      b: '\\({7.09^o}\\)<br>',
+      c: '\\({82.91^o}\\)<br>',
+      d: '\\({61.01^o}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "11. Determine \\({angle}~{B}\\) to the nearest tenth of a degree when \\({sin}~{B}=0.3548\\).",
+    answers: {
+      a: '\\({69.2^o}\\)<br>',
+      b: '\\({20.8^o}\\)<br>',
+      c: '\\({19.5^o}\\)<br>',
+      d: '\\({70.5^o}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "12. Find \\({angle}~{C}\\) to the nearest hundredth of a degree when \\({tan}~{C}=1.5467\\).",
+    answers: {
+      a: '\\({33.15^o}\\)<br>',
+      b: '\\({56.85^o}\\)<br>',
+      c: '\\({57.12^o}\\)<br>',
+      d: '\\({32.88^o}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "<h4>13. Determine \\({angle}~{B}\\) to the nearest tenth of a degree in the given right triangle below. </h4> <img src='images/4_triangle.png' width='150' height='190'>",
+    answers: {
+      a: '\\({59.9^o}\\)<br>',
+      b: '\\({60.0^o}\\)<br>',
+      c: '\\({30.1^o}\\)<br>',
+      d: '\\({30.0^o}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "<h4>14. Determine \\({angle}~{S}\\) to the nearest degree in the given right triangle below. </h4> <img src='images/5_triangle.png' width='150' height='150'>",
+    answers: {
+      a: '\\({35^o}\\)<br>',
+      b: '\\({40^o}\\)<br>',
+      c: '\\({45^o}\\)<br>',
+      d: '\\({50^o}\\)'
+    },
+    correctAnswer: 'c'
+  },
+
+  {
+    question: "<h4>15. Determine \\({angle}~{X}\\) to the nearest tenth of degree in the given right triangle below. </h4> <img src='images/6_triangle.png' width='300' height='100'>",
+    answers: {
+      a: '\\({12.7^o}\\)<br>',
+      b: '\\({62.9^o}\\)<br>',
+      c: '\\({27.1^o}\\)<br>',
+      d: '\\({77.3^o}\\)'
+    },
+    correctAnswer: 'a'
+  },
+
+  {
+    question: "<h4>16. Determine the measure of side \\({XY}\\) on the triangle below. Round your answer to four significant digits.</h4> <img src='images/7_triangle.png' width='300' height='120'>",
+    answers: {
+      a: '\\({45.56}~{m}\\)<br>',
+      b: '\\({18.46}~{m}\\)<br>',
+      c: '\\({19.98}~{m}\\)<br>',
+      d: '\\({23.68}~{m}\\)'
+    },
+    correctAnswer: 'b'
+  },
+
+  {
+    question: "<h4>17. Determine the measure of side \\({AB}\\) on the triangle below. Round your answer to three significant digits.</h4> <img src='images/8_triangle.png' width='220' height='150'>",
+    answers: {
+      a: '\\({101}~{m}\\)<br>',
+      b: '\\({92.6}~{m}\\)<br>',
+      c: '\\({74.5}~{m}\\)<br>',
+      d: '\\({46.1}~{m}\\)'
+    },
+    correctAnswer: 'a'
+  },
 ];
 
 var quizContainer = document.getElementById('quiz');
